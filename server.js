5bc794ba81972f0a3f54764c1509252fc7cbde53
Unknown <rome.1205h@gmail.com>
2019-07-23 02:16:20 -0500

    22/07/19 pug, gulp y generacion de public dinamico

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));   
+
 app.get('/', function(req, res){
-    res.send('Hola Mundillo');
+    res.render('index');
 })
 
 app.listen(3000, function(err){
