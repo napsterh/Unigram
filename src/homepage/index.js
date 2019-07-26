d4b0896e02204d8401c8f8cb9583ec2debe5e3aa
Unknown <rome.1205h@gmail.com>
2019-07-26 02:48:03 -0500

    Fecha de publicación con moment.js

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,7 +15,8 @@ page('/', function(ctx, next){
             },
             url:'office.jpg',
             likes: 10,
-            liked: false
+            liked: false,
+            createdAt: new Date()
         },
         {
             user: {
@@ -24,7 +25,8 @@ page('/', function(ctx, next){
             },
             url:'office.jpg',
             likes: 2,
-            liked: true
+            liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
         }
     ];
 
--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,4 +1,5 @@
 var yo = require('yo-yo');
+var moment = require('moment');
 
 module.exports = function pictureCard(pic){
     var el;
@@ -13,7 +14,7 @@ module.exports = function pictureCard(pic){
                 <img src="${picture.user.avatar}" class="avatar" />
                 <span class="username">${picture.user.username}</span>
             </a>
-            <small class="right time">Hace 1 dia</small>
+            <small class="right time">${moment(picture.createdAt).fromNow()}</small>
             <p>
                 <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
                 <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
