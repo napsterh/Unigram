ddd224349c78547136190d880b8989183918e8db
HOMER-PC <rome.1205h@gmail.com>
2019-10-13 00:52:44 -0500

    respuesta del servidor con fetch API

--- public/app.js
+++ public/app.js
@@ -20572,7 +20572,7 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, loadPictures, function (ctx, next) {
+page('/', header, loadPicturesFetch, function (ctx, next) {
   title('Unigram');
   var main = document.getElementById('main-container');
 
@@ -20591,6 +20591,17 @@ function loadPictures(ctx, next) {
 function loadPicturesAxios(ctx, next) {
   axios.get('/api/pictures').then(function (res) {
     ctx.pictures = res.data;
+    next();
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+function loadPicturesFetch(ctx, next) {
+  fetch('/api/pictures').then(function (res) {
+    return res.json();
+  }).then(function (pictures) {
+    ctx.pictures = pictures;
     next();
   }).catch(function (err) {
     console.log(err);
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,7 +6,7 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, loadPictures, function(ctx, next){
+page('/', header, loadPicturesFetch, function(ctx, next){
     title('Unigram');
     var main = document.getElementById('main-container');
 
@@ -36,3 +36,18 @@ function loadPicturesAxios(ctx, next){
       console.log(err);
     })
 }
+
+
+function loadPicturesFetch(ctx, next){
+  fetch('/api/pictures')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (pictures){
+      ctx.pictures = pictures;
+      next();
+    })
+    .catch(function (err){
+      console.log(err);
+    })
+} 
